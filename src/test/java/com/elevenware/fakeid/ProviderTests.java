88f9dd563f289f300a03804808a62be2dd54558d
George McIntosh <george@georgemcintosh.com>
2025-06-22 21:41:57 +0100

    wip

--- src/test/java/com/elevenware/fakeid/ProviderTests.java
+++ src/test/java/com/elevenware/fakeid/ProviderTests.java
@@ -4,19 +4,25 @@ import io.javalin.http.Context;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 public class ProviderTests {
 
     @Test
     void testDiscoveryDocument() {
-        FakeIdProvider provider = new FakeIdProvider(Configuration.defaultConfiguration());
+
+        Configuration configuration = Configuration.defaultConfiguration();
+        FakeIdProvider provider = new FakeIdProvider(configuration);
 
         Context ctx = mock(Context.class);
-        // capture json call
-        ArgumentCaptor<?> jsonCaptor = ArgumentCaptor.forClass(Object.class);
+
+        ArgumentCaptor<DiscoveryDocument> jsonCaptor = ArgumentCaptor.forClass(DiscoveryDocument.class);
         provider.getDiscoveryDocument(ctx);
+        verify(ctx, times(1)).json(jsonCaptor.capture());
+        DiscoveryDocument doc = jsonCaptor.getValue();
+
+        assertEquals(doc.getIssuer(), configuration.getIssuer());
     }
 
 }
