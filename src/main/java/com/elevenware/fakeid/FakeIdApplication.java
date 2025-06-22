64e3e81b68a975accf2ea884d529118f60f680f6
George McIntosh <george@georgemcintosh.com>
2025-06-22 18:25:09 +0100

    Renaming

--- src/main/java/com/elevenware/fakeid/FakeIdApplication.java
+++ src/main/java/com/elevenware/fakeid/FakeIdApplication.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import io.javalin.Javalin;
 import io.javalin.json.JavalinJackson;
 
-public class EasyIdApplication {
+public class FakeIdApplication {
 
     public static void main(String[] args) {
 
