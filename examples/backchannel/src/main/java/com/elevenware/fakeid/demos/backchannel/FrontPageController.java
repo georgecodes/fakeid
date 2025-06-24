10829976bfe3ac5bbdd4f4f6ffdd517ad74546f5
George McIntosh <george@georgemcintosh.com>
2025-06-24 07:42:18 +0100

    wip

--- examples/backchannel/src/main/java/com/elevenware/fakeid/demos/backchannel/FrontPageController.java
+++ examples/backchannel/src/main/java/com/elevenware/fakeid/demos/backchannel/FrontPageController.java
@@ -1,5 +1,6 @@
 package com.elevenware.fakeid.demos.backchannel;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,7 +9,7 @@ import org.springframework.web.servlet.ModelAndView;
 public class FrontPageController {
 
     @GetMapping("/")
-    public ModelAndView frontPage() {
+    public ModelAndView frontPage(@AuthenticationPrincipal AuthenticationPrincipal principal) {
         ModelAndView modelAndView = new ModelAndView("index");
         modelAndView.addObject("title", "Fake ID Backchannel Demo");
         modelAndView.addObject("message", "Welcome to the Fake ID Backchannel Demo!");
